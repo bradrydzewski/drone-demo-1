f27e9b77d8763d3281d722c5e1e006a13966047a
Brad Rydzewski <brad.rydzewski@gmail.com>
2020-08-13 09:23:09 -0400

    first commit

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestOne(t *testing.T) {
+  time.Sleep(time.Second)
+}
+
+func TestTwo(t *testing.T) {
+  time.Sleep(time.Second)
+}
+
+func TestThree(t *testing.T) {
+  time.Sleep(time.Second)
+}
+
+func TestFour(t *testing.T) {
+  time.Sleep(time.Second)
+}
